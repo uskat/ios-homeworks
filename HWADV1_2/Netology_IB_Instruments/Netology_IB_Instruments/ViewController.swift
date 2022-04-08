a1a82efc2126265dc2cc0b46597ca8ee9522f518
uskat <uskat@rambler.ru>
2022-04-08 18:03:11 +0200

    initial commit

---
+++ HWADV1_2/Netology_IB_Instruments/Netology_IB_Instruments/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netology_IB_Instruments
+//
+//  Created by Андрей Абрамов on 3/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
