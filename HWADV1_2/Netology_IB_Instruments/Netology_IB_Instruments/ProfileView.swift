3770d7a8179b385493ec5023358abb5b9e5f0738
uskat <uskat@rambler.ru>
2022-04-08 22:43:02 +0200

    first comit

---
+++ HWADV1_2/Netology_IB_Instruments/Netology_IB_Instruments/ProfileView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by Андрей Абрамов on 8/4/22.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
