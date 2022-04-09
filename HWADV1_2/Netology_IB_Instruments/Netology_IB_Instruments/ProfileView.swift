9f0c4914b278e5cf3c262775d98021d1f4291a83
uskat <uskat@rambler.ru>
2022-04-09 04:46:09 +0200

    hometask 1_2

--- HWADV1_2/Netology_IB_Instruments/Netology_IB_Instruments/ProfileView.swift
+++ HWADV1_2/Netology_IB_Instruments/Netology_IB_Instruments/ProfileView.swift
@@ -1,20 +1,11 @@
-//
-//  ProfileView.swift
-//  Netology_IB_Instruments
-//
-//  Created by Андрей Абрамов on 8/4/22.
-//
-
 import UIKit
 
 class ProfileView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
-
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var birthdayLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var photoView: UIImageView!
+    @IBOutlet weak var signView: UITextView!
+    
 }
